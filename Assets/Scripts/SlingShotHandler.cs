e5f966ff7e01edaa216985d42a16d01ef85a5ee5
zkdlfl <159306744+zkdlfl@users.noreply.github.com>
2024-10-22 11:03:28 -0400

    Created Slingshot to Follow Mouse Movement

--- Assets/Scripts/SlingShotHandler.cs
+++ Assets/Scripts/SlingShotHandler.cs
@@ -1,10 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class SlingShotHandler : MonoBehaviour
 {
+    [SerializeField] private LineRenderer _leftLineRenderer;
+    [SerializeField] private LineRenderer _rightLineRenderer;
+
+    [SerializeField] private Transform _leftStartPosition;
+    [SerializeField] private Transform _rightStartPosition;
+
     private void Update(){
-        
+        if(Mouse.current.leftButton.isPressed){
+            DrawSlingShot();
+        }
+        // Debug.Log(Mouse.current.position.ReadValue());
+    }
+    private void DrawSlingShot(){
+        Vector3 touchposition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        SetLines(touchposition);
+    }
+    private void SetLines(Vector2 position){
+        Debug.Log(position);
+        _leftLineRenderer.SetPosition(3, position);
+        _leftLineRenderer.SetPosition(1, _leftStartPosition.position);
+
+        _rightLineRenderer.SetPosition(3, position);
+        _rightLineRenderer.SetPosition(1, _rightStartPosition.position);
     }
 }
