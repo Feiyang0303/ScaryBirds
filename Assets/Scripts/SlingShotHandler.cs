b9def506f7f478f35a9884a897fe0b73e4ce9459
zkdlfl <159306744+zkdlfl@users.noreply.github.com>
2024-10-23 10:56:09 -0400

    Fixed Slingshot Restriction

--- Assets/Scripts/SlingShotHandler.cs
+++ Assets/Scripts/SlingShotHandler.cs
@@ -9,18 +9,30 @@ using Vector3 = UnityEngine.Vector3;
 
 public class SlingShotHandler : MonoBehaviour
 {
+    [Header("Line Renderers")] 
     [SerializeField] private LineRenderer _leftLineRenderer;
     [SerializeField] private LineRenderer _rightLineRenderer;
 
+
+    [Header("Transform References")]
     [SerializeField] private Transform _leftStartPosition;
     [SerializeField] private Transform _rightStartPosition;
     [SerializeField] private Transform _centerPosition;
     [SerializeField] private Transform _idlePosition;
+
+    [Header("Slingshot Stats")]
     [SerializeField] private float _maxDistance = 3.5f;
+
+    [Header("Scripts")]
     [SerializeField] private SlingShotArea _slingshotArea;
     private Vector2 _slingShotLinesPosition;
     private bool _clickedWithinArea;
-
+    
+    // -------------------------------------------------------
+    private void Awake(){
+        _leftLineRenderer.enabled = false;
+        _rightLineRenderer.enabled = false;
+    }
     private void Update(){
         if(Mouse.current.leftButton.wasPressedThisFrame && _slingshotArea.IsWithinSlingShotArea()){
             _clickedWithinArea = true;
@@ -34,6 +46,10 @@ public class SlingShotHandler : MonoBehaviour
         // Debug.Log(Mouse.current.position.ReadValue());
     }
     private void DrawSlingShot(){
+        if(!_leftLineRenderer.enabled && !_rightLineRenderer.enabled){
+            _leftLineRenderer.enabled = true;
+            _rightLineRenderer.enabled = true;
+        }
         Vector3 touchposition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         _slingShotLinesPosition = _centerPosition.position + Vector3.ClampMagnitude(touchposition - _centerPosition.position, _maxDistance);
         SetLines(_slingShotLinesPosition);
