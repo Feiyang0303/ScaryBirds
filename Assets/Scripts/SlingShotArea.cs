a0624d7b3a3994a665bd0c5d9e18afddcf80f3e3
zkdlfl <159306744+zkdlfl@users.noreply.github.com>
2024-10-23 10:32:10 -0400

    Added Restriction in the area of controlling the slingshot

---
+++ Assets/Scripts/SlingShotArea.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SlingShotArea : MonoBehaviour
+{
+    [SerializeField] private LayerMask _slingshotAreaMask;
+    public bool IsWithinSlingShotArea(){
+        UnityEngine.Vector2 worldPosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        if(Physics2D.OverlapPoint(worldPosition, _slingshotAreaMask)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    } 
+}
--- Assets/Scripts/SlingShotHandler.cs
+++ Assets/Scripts/SlingShotHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Vector2 = UnityEngine.Vector2;
@@ -13,16 +14,30 @@ public class SlingShotHandler : MonoBehaviour
 
     [SerializeField] private Transform _leftStartPosition;
     [SerializeField] private Transform _rightStartPosition;
+    [SerializeField] private Transform _centerPosition;
+    [SerializeField] private Transform _idlePosition;
+    [SerializeField] private float _maxDistance = 3.5f;
+    [SerializeField] private SlingShotArea _slingshotArea;
+    private Vector2 _slingShotLinesPosition;
+    private bool _clickedWithinArea;
 
     private void Update(){
-        if(Mouse.current.leftButton.isPressed){
+        if(Mouse.current.leftButton.wasPressedThisFrame && _slingshotArea.IsWithinSlingShotArea()){
+            _clickedWithinArea = true;
+        }
+        if(Mouse.current.leftButton.isPressed && _clickedWithinArea){
             DrawSlingShot();
+        }
+        if(Mouse.current.leftButton.wasReleasedThisFrame){
+            _clickedWithinArea = false;
         }
         // Debug.Log(Mouse.current.position.ReadValue());
     }
     private void DrawSlingShot(){
         Vector3 touchposition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        SetLines(touchposition);
+        _slingShotLinesPosition = _centerPosition.position + Vector3.ClampMagnitude(touchposition - _centerPosition.position, _maxDistance);
+        SetLines(_slingShotLinesPosition);
+
     }
     private void SetLines(Vector2 position){
         Debug.Log(position);
